2e92af7e34303b1e8091780998ced60f59e04a36
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2022-09-19 10:17:06 +0800

    Add FD extension validation (#11)
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- extensions/extensions_validator.c
+++ extensions/extensions_validator.c
@@ -52,12 +52,60 @@ static inline s_validate_result_t validate_M_extension() {
   return result;
 }
 
+static inline s_validate_result_t validate_F_extension() {
+  float a = 41.2f, b = 92.24f, c;
+  s_validate_result_t result = {
+      true,
+      "F",
+      "Extension F is Supported",
+  };
+
+  asm volatile("flw        f0, %[src1]\n\t"
+               "flw        f1, %[src2]\n\t"
+               "fsub.s     f2, f1, f0\n\t"
+               "fadd.s     f3, f1, f0\n\t"
+               "fsqrt.s    f4, f3\n\t"
+               "fmax.s     f5, f4, f2\n\t"
+               "fsw        f5, %[dest]\n\t"
+               "fence"
+               : [dest] "=m"(c)
+               : [src1] "m"(a), [src2] "m"(b)
+               :);
+
+  return result;
+}
+
+static inline s_validate_result_t validate_D_extension() {
+  double a = 41.2f, b = 92.24f, c;
+  s_validate_result_t result = {
+      true,
+      "D",
+      "Extension D is Supported",
+  };
+
+  asm volatile("fld        f0, %[src1]\n\t"
+               "fld        f1, %[src2]\n\t"
+               "fsub.d     f2, f1, f0\n\t"
+               "fadd.d     f3, f1, f0\n\t"
+               "fsqrt.d    f4, f3\n\t"
+               "fmax.d     f5, f4, f2\n\t"
+               "fsd        f5, %[dest]\n\t"
+               "fence"
+               : [dest] "=m"(c)
+               : [src1] "m"(a), [src2] "m"(b)
+               :);
+
+  return result;
+}
+
 s_validate_result_collection_t *validate_extensions() {
   s_validate_result_collection_t *collection =
       create_result_collection(EXTENSIONS_MAX_CAPACITY);
 
   append_one_result(collection, validate_I_extension());
   append_one_result(collection, validate_M_extension());
+  append_one_result(collection, validate_F_extension());
+  append_one_result(collection, validate_D_extension());
 
   return collection;
 }
